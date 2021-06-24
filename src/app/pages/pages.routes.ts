2a1c09315a608b37ced4f5472035139ed8d96eed
Rafa <rafacbapython@gmail.com>
2021-06-24 17:56:55 -0300

    Modulos agregados

---
+++ src/app/pages/pages.routes.ts
@@ -0,0 +1,29 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { PagesComponent } from './pages.component';
+import { ProgressComponent } from './progress/progress.component';
+
+
+const pagesRoutes: Routes = [
+    { 
+        path: '', 
+        component: PagesComponent,
+        children:[
+            { path: 'dashboard', component: DashboardComponent },
+            { path: 'progress', component: ProgressComponent },
+            { path: 'graficas1', component: Graficas1Component },
+            { path: '', pathMatch: 'full', redirectTo:'/dashboard' }
+        ]
+    }
+    
+];
+
+// @NgModule({
+//     imports: [RouterModule.forChild(pagesRoutes)],
+//     exports: [RouterModule]
+// })
+// export class PagesRoutingModule {}
+
+export const PAGES_ROUTES = RouterModule.forChild(pagesRoutes);
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from "@angular/core";
+import { BreadcrumbsComponent } from "./breadcrumbs/breadcrumbs.component";
+import { HeaderComponent } from "./header/header.component";
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { SidebarComponent } from "./sidebar/sidebar.component";
+
+
+@NgModule({
+    declarations: [
+        HeaderComponent,
+        SidebarComponent,
+        BreadcrumbsComponent,
+        NopagefoundComponent
+    ],
+    exports: [
+        HeaderComponent,
+        SidebarComponent,
+        BreadcrumbsComponent,
+        NopagefoundComponent
+    ]
+})
+export class SharedModule {}
+
